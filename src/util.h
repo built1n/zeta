05172639bcfafc7de94241fef04dfa5cba1daa08
Franklin Wei <frankhwei536@gmail.com>
2014-03-07 17:24:31 -0500

    Added platform-specific print macros for easier portability

--- src/util.h
+++ src/util.h
@@ -6,4 +6,14 @@ void badWrite(zeta_ctx* ctx);
 void badRead(zeta_ctx* ctx);
 void stackOverflow(zeta_ctx* ctx);
 void stackUnderflow(zeta_ctx* ctx);
+void badInstr(zeta_ctx*);
+
+#define printInt(a) printf("%d", (a))
+#define printHex_lcase(a) printf("%x", (a))
+#define printHex_ucase(a) printf("%X", (a))
+#define printString(a) printf("%s", (a))
+#define printChar(a) putchar(a)
+void printBinary(uint32_t);
+#define likely(x) __builtin_expect((x),1)
+#define unlikely(x) __builtin_expect((x),0)
 #endif
