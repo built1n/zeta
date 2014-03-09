764fa6cf6179f809010a86a43a67ef2207724c2d
Franklin Wei <frankhwei536@gmail.com>
2014-03-09 18:11:04 -0400

    Added GPLv3 header

--- src/zeta.c
+++ src/zeta.c
@@ -1,8 +1,21 @@
 /*
- * zeta.c
- * Utility functions, the language implementation is in exec.c
- *  Created on: Mar 3, 2014
- *      Author: franklin
+ *  Zeta emulation library
+ *  Copyright (C) 2014 Franklin Wei
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Utility functions, the language implementation is in exec.c
  */
 #include <zeta.h>
 #include <util.h>
