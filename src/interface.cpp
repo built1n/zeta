490d449188166e1a8a12a1d767d22c0e30e32d54
Franklin Wei <frankhwei536@gmail.com>
2014-03-09 17:05:34 -0400

    Improved interface

--- src/interface.cpp
+++ src/interface.cpp
@@ -1,65 +1,47 @@
 // A C++ interface for Zeta
-// rather non-optimal code
+// rather buggy code
+#include <fstream>
 #include <iostream>
 #include <zeta.h>
 #include <string>
 #include <cstdlib>
 #include <vector>
 #include <cctype>
+#include <cstdio>
 using namespace std;
 char hex_chars[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-int main()
+int main(int argc, char* argv[])
 {
-  vector<byte> program;
-  cout << "Enter program in hex:" << endl;
-  while(cin)
+  vector<byte> prog;
+  while(cin.good())
     {
       string line;
       getline(cin, line);
-      if(line.length()%2!=0 and line!="")
-	{
-	  cerr << "Syntax error!" << endl;
-	  return -1;
-	}
       for(int i=0;i<line.length();i+=2)
 	{
-	  byte val;
-	  bool good=false;
-	  string oneByte=line.substr(i, i+2);
+	  byte val=0xFF; 
 	  for(int j=0;j<16;++j)
 	    {
-	      if(toupper(oneByte[0])==hex_chars[j])
+	      if(line[i+1]==hex_chars[j])
 		{
 		  val=j;
-		  good=true;
 		}
 	    }
-	  if(!good)
-	    {
-	      cerr << "Non-hex character found!" << endl;
-	      return -1;
-	    }
-	  good=false;
 	  for(int j=0;j<16;++j)
 	    {
-	      if(toupper(oneByte[1])==hex_chars[j])
+	      if(line[i]==hex_chars[j])
 		{
 		  val|=((j&0xF)<<4);
-		  good=true;
 		}
 	    }
-	  if(!good)
-	    {
-	      cerr << "Non-hex character found!" << endl;
-	      return -1;
-	    }
-	  program.push_back(val);
+	  prog.push_back(val);
+	  printf("Got 0x%02X\n", val);
 	}
     }
-  byte* p=(byte*)malloc(program.size()+2048); // add 2048 byte stack
-  for(int i=0;i<program.size();++i)
-    p[i]=program[i];
-  zeta_ctx* ctx=zeta_init((byte*)p, program.size()+2048,2048,program.size(), true);
+  byte* p=(byte*)malloc(prog.size()+2048);
+  for(int i=0;i<prog.size();++i)
+    p[i]=prog[i];
+  zeta_ctx* ctx=zeta_init((byte*)p, prog.size(),prog.size(),0, true);
   while(!ctx->done)
     zeta_exec(ctx);
   return ctx->return_value;
